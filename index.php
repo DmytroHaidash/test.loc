177494051f71b1390ccfa413629cc7df000bdf10
DmytroHaidash <dmytro.haidash@gmail.com>
2018-08-02 20:45:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Надюшечка
+ * Date: 02.08.2018
+ * Time: 20:15
+ */
+echo 'Hellow World';
+//print_r ('Hello Piter');
+//var_dump ('hello Nadin');
+
+
+//echo phpinfo();
+
+?>
